a4240abec73da53622baca3895ca6775cb831240
Jon Steele <jonwhsteele@gmail.com>
2023-08-05 10:32:21 -0700

    Adds labels for form inputs

--- src/form.js
+++ src/form.js
@@ -25,98 +25,105 @@ import {
 } from "@coreui/react";
 
 const Form = () => {
-    const { handleSubmit, control, formState: { errors }, reset, watch, setValue } = useForm();
-    const [users, setUsers] = useState([]);
-    const [editIndex, setEditIndex] = useState(-1);
-    const [country, setCountry] = useState("Canada");
-    const watchCountry = watch("country", "Canada");
-  
-    useEffect(() => {
-      setCountry(watchCountry);
-  
-      const usersCollectionRef = collection(db, "users");
-      const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
-        const data = snapshot.docs.map((doc) => {
-          const data = doc.data();
-          if (data.DOB && typeof data.DOB.toDate === "function") {
-            data.DOB = data.DOB.toDate().toISOString().split("T")[0];
-          }
-          return { id: doc.id, ...data };
-        });
-        setUsers(data);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+    watch,
+    setValue,
+  } = useForm();
+  const [users, setUsers] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+  const [country, setCountry] = useState("Canada");
+  const watchCountry = watch("country", "Canada");
+
+  useEffect(() => {
+    setCountry(watchCountry);
+
+    const usersCollectionRef = collection(db, "users");
+    const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
+      const data = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        if (data.DOB && typeof data.DOB.toDate === "function") {
+          data.DOB = data.DOB.toDate().toISOString().split("T")[0];
+        }
+        return { id: doc.id, ...data };
       });
-  
-      return () => unsubscribe();
-    }, [watchCountry]);
-  
-    const onSubmit = async (data) => {
-      try {
-        let dob = new Date(`${data.dateOfBirth}T00:00:00Z`);
-        dob.setHours(dob.getHours() + 8);
-        data.dateOfBirth = Timestamp.fromDate(dob);
-  
-        if (typeof data.country === "string") {
-          data.country = { value: data.country, label: data.country };
-        }
-        if (typeof data.city === "string") {
-          data.city = { value: data.city, label: data.city };
-        }
-  
-        const usersCollectionRef = collection(db, "users");
-  
-        if (editIndex !== -1) {
-          const docRef = doc(db, "users", users[editIndex].id);
-          await updateDoc(docRef, data);
-        } else {
-          await addDoc(usersCollectionRef, data);
-        }
-  
-        reset();
+      setUsers(data);
+    });
+
+    return () => unsubscribe();
+  }, [watchCountry]);
+
+  const onSubmit = async (data) => {
+    try {
+      let dob = new Date(`${data.dateOfBirth}T00:00:00Z`);
+      dob.setHours(dob.getHours() + 8);
+      data.dateOfBirth = Timestamp.fromDate(dob);
+
+      if (typeof data.country === "string") {
+        data.country = { value: data.country, label: data.country };
+      }
+      if (typeof data.city === "string") {
+        data.city = { value: data.city, label: data.city };
+      }
+
+      const usersCollectionRef = collection(db, "users");
+
+      if (editIndex !== -1) {
+        const docRef = doc(db, "users", users[editIndex].id);
+        await updateDoc(docRef, data);
+      } else {
+        await addDoc(usersCollectionRef, data);
+      }
+
+      reset();
+      setEditIndex(-1);
+    } catch (error) {
+      console.error("Error submitting form: ", error);
+    }
+  };
+
+  const handleDelete = async (index) => {
+    try {
+      const userToDelete = users[index];
+      const userDocRef = doc(db, "users", userToDelete.id);
+      await deleteDoc(userDocRef);
+
+      const updatedUsers = [...users];
+      updatedUsers.splice(index, 1);
+      setUsers(updatedUsers);
+      reset();
+
+      if (editIndex === index) {
         setEditIndex(-1);
-      } catch (error) {
-        console.error("Error submitting form: ", error);
       }
+    } catch (error) {
+      console.error("Error deleting user: ", error);
+    }
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    const userToEdit = users[index];
+    const dob = userToEdit.dateOfBirth.toDate().toISOString().split("T")[0];
+
+    const userFormValues = {
+      name: userToEdit.name,
+      dateOfBirth: dob,
+      country: userToEdit.country,
+      city: userToEdit.city,
     };
-  
-    const handleDelete = async (index) => {
-      try {
-        const userToDelete = users[index];
-        const userDocRef = doc(db, "users", userToDelete.id);
-        await deleteDoc(userDocRef);
-  
-        const updatedUsers = [...users];
-        updatedUsers.splice(index, 1);
-        setUsers(updatedUsers);
-        reset();
-  
-        if (editIndex === index) {
-          setEditIndex(-1);
-        }
-      } catch (error) {
-        console.error("Error deleting user: ", error);
-      }
-    };
-  
-    const handleEdit = (index) => {
-      setEditIndex(index);
-      const userToEdit = users[index];
-      const dob = userToEdit.dateOfBirth.toDate().toISOString().split("T")[0];
-  
-      const userFormValues = {
-        name: userToEdit.name,
-        dateOfBirth: dob,
-        country: userToEdit.country,
-        city: userToEdit.city,
-      };
-  
-      reset(userFormValues);
-    };
+
+    reset(userFormValues);
+  };
 
   return (
     <div className="d-flex flex-column">
       <CForm
         onSubmit={handleSubmit(onSubmit)}
-        className="w-75 m-auto d-flex flex-column"
+        className="col-12 col-md-9 m-auto d-flex flex-column mt-3"
       >
         <Controller
           name="name"
@@ -124,7 +131,14 @@ const Form = () => {
           rules={{ required: "Name is required" }}
           defaultValue=""
           render={({ field }) => (
-            <CFormInput className="my-2" {...field} placeholder="Name" />
+            <>
+              <label htmlFor="name">Name</label>
+              <CFormInput
+                className="my-2"
+                {...field}
+                placeholder="John Smith"
+              />
+            </>
           )}
         />
         {errors.name && <p>{errors.name.message}</p>}
@@ -135,37 +149,43 @@ const Form = () => {
           rules={{ required: "Date of birth is required" }}
           defaultValue=""
           render={({ field }) => (
-            <CFormInput className="my-2" type="date" {...field} />
+            <>
+              <label htmlFor="dateOfBirth">Date of Birth</label>
+              <CFormInput className="my-2" type="date" {...field} />
+            </>
           )}
         />
-        {errors.dateOfBirth && (<p>{errors.dateOfBirth.message}</p>)}
+        {errors.dateOfBirth && <p>{errors.dateOfBirth.message}</p>}
 
         <Controller
           name="country"
           control={control}
           defaultValue={{ value: "CA", label: "Canada" }}
           render={({ field }) => (
-            <Select
-              className="my-2 w-100"
-              {...field}
-              options={[
-                { value: "CA", label: "Canada" },
-                { value: "US", label: "USA" },
-              ]}
-              isSearchable
-              onChange={(selectedOption) => {
-                setCountry(selectedOption.value);
+            <>
+              <label htmlFor="country">Country</label>
+              <Select
+                className="my-2 w-100"
+                {...field}
+                options={[
+                  { value: "CA", label: "Canada" },
+                  { value: "US", label: "USA" },
+                ]}
+                isSearchable
+                onChange={(selectedOption) => {
+                  setCountry(selectedOption.value);
 
-                // Reset city when country changes
-                if (selectedOption.value === "CA") {
-                  setValue("city", { value: "OT", label: "Ottawa" });
-                } else {
-                  setValue("city", { value: "LV", label: "Las Vegas" });
-                }
+                  // Reset city when country changes
+                  if (selectedOption.value === "CA") {
+                    setValue("city", { value: "OT", label: "Ottawa" });
+                  } else {
+                    setValue("city", { value: "LV", label: "Las Vegas" });
+                  }
 
-                field.onChange(selectedOption);
-              }}
-            />
+                  field.onChange(selectedOption);
+                }}
+              />
+            </>
           )}
         />
 
@@ -174,23 +194,26 @@ const Form = () => {
           control={control}
           defaultValue={{ value: "OT", label: "Ottawa" }}
           render={({ field }) => (
-            <Select
-              className="my-2 w-100"
-              {...field}
-              options={
-                country.label === "Canada"
-                  ? [
-                      { value: "OT", label: "Ottawa" },
-                      { value: "TO", label: "Toronto" },
-                    ]
-                  : [
-                      { value: "LV", label: "Las Vegas" },
-                      { value: "CH", label: "Chicago" },
-                    ]
-              }
-              isSearchable
-              onChange={field.onChange}
-            />
+            <>
+              <label htmlFor="city">City</label>
+              <Select
+                className="my-2 w-100"
+                {...field}
+                options={
+                  country.label === "Canada"
+                    ? [
+                        { value: "OT", label: "Ottawa" },
+                        { value: "TO", label: "Toronto" },
+                      ]
+                    : [
+                        { value: "LV", label: "Las Vegas" },
+                        { value: "CH", label: "Chicago" },
+                      ]
+                }
+                isSearchable
+                onChange={field.onChange}
+              />
+            </>
           )}
         />
         {errors !== undefined && errors.name && <p>{errors.city.message}</p>}
