2647f8974b4f41f4b09ec061d808016341a163d6
Jon Steele <jonwhsteele@gmail.com>
2023-08-05 10:05:38 -0700

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import "./App.css";
-import "@coreui/coreui/dist/css/coreui.min.css";
 import Form from "./form";
-import { FormProvider, useForm } from "react-hook-form";
 
 const App = () => {
-  const methods = useForm();
   return (
     <div>
-      <FormProvider {...methods}>
         <Form />
-      </FormProvider>
     </div>
   );
 };
